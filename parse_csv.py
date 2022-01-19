16c2d15d1c374814f6887d6c52b8f53667f8b8d0
Ifme87 <if.me@mail.ru>
2022-01-19 17:07:05 +0300

    done

--- parse_csv.py
+++ parse_csv.py
@@ -1,7 +1,8 @@
 import csv
 from pprint import pprint
 
-'''Сравнивает наличие hostname/ip из csv файла в файле hosts_tmp
+'''Сравнивает наличие hostname/ip из csv файла в файле hosts_tmp,
+    выводит результат, которого нет в hosts_tmp
 '''
 def parse_inventory(csv_file):
     with open(csv_file, encoding="utf-16", newline="") as f:
